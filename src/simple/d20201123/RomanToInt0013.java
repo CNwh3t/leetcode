639f4a8231c39e971b0c334dd14ee4d1e8aff79d
yanfa-1 <snszwh@163.com>
2020-11-24 18:12:32 +0800

    罗马数字转整数(自解)

--- src/simple/d20201123/RomanToInt0013.java
+++ src/simple/d20201123/RomanToInt0013.java
@@ -1,5 +1,8 @@
 package simple.d20201123;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author wh
  * @version 1.0
@@ -48,11 +51,38 @@ public class RomanToInt0013 {
     //    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
     public static int romanToInt(String s) {
-        
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
 
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char charAtI = s.charAt(i);
+            if (!map.containsKey(charAtI)) {
+                return 0;
+            }
+            // i和i+
+            Integer intI = map.get(charAtI);
+            Integer intIPlus = 0;
+            if (i != s.length() -1) {
+                intIPlus = map.get(s.charAt(i + 1));
+            }
+            if (intI >= intIPlus) {
+                result += intI ;
+            }else {
+                result -= intI;
+            }
+        }
+        return result;
+    }
 
-
-        return 0;
+    public static void main(String[] args) {
+        System.out.println(romanToInt("XLIX"));
     }
 
 }
